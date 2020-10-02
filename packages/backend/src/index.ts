f7c4066de3da17fd666a981a57870991169ebc79
Lucemans <luc@lucemans.nl>
2020-10-02 14:41:50 +0200

    Fixed Oopsy

--- packages/backend/src/index.ts
+++ packages/backend/src/index.ts
@@ -1,1 +1,14 @@
-console.log('hi');
+import express, { Request, Response } from 'express';
+
+const app = express();
+const port = 8080;
+
+const returnURL: { [key: string]: string } = {};
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('BACKEND OK');
+});
+
+app.listen(port, () => {
+    console.log('ONLINE');
+});
