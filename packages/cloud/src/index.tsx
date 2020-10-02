1f9a23e42b7e2aa169c8c1c3f5e7ea1725f1faf5
Lucemans <luc@lucemans.nl>
2020-10-02 18:42:01 +0200

    Version and Dockerignore

--- packages/cloud/src/index.tsx
+++ packages/cloud/src/index.tsx
@@ -1,4 +1,3 @@
-import { exit } from 'process';
 import * as React from 'react';
 import { render } from 'react-dom';
 import App from './App';
@@ -7,20 +6,22 @@ import buildUrl from 'build-url';
 
 console.log('Version: ' + process.env.npm_package_version);
 
-if (location.search) {
-    const res = queryString.parse(location.search);
-    if (res['auth']) {
-        sessionStorage.setItem('token', res['auth'].toString());
-        location.href = (buildUrl(location.protocol + "//" + location.host + location.pathname));
-    } else {
-        console.error('Gotten unknown search');
+((a) => {
+    if (location.search) {
+        const res = queryString.parse(location.search);
+        if (res['auth']) {
+            sessionStorage.setItem('token', res['auth'].toString());
+            location.href = (buildUrl(location.protocol + "//" + location.host + location.pathname));
+        } else {
+            console.error('Gotten unknown search');
+        }
     }
-}
-if (!sessionStorage.getItem('token')) {
-    location.replace('https://auth.lvk.sh/login?callback=' + location.toString());
-}
-
-// Render root
-var root = document.getElementById("root");
-
-render(<App></App>, root);
+    if (!sessionStorage.getItem('token')) {
+        location.replace('https://auth.lvk.sh/login?callback=' + location.toString());
+    }
+    
+    // Render root
+    var root = document.getElementById("root");
+    
+    render(<App></App>, root);
+})();
